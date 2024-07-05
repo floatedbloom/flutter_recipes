771d9da7ea4560b5eb2b44533a659487b4d26fcf
George Lapin <georgelapin23@gmail.com>
2024-07-04 19:09:24 -0700

    a little craetion progress

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_recipes/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 //cant create a recipe with a name you already used
 //show creation screen
@@ -11,10 +14,110 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
+  final _formKey = GlobalKey<FormState>();
+  final DatabaseHelper dbHelper = DatabaseHelper();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _creatorController = TextEditingController();
+  final TextEditingController _ingredientsController = TextEditingController();
+  final TextEditingController _healthController = TextEditingController();
+  final TextEditingController _typeController = TextEditingController();
+  final TextEditingController _dietController = TextEditingController();
+  final TextEditingController _imageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: "Recipe Name"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Name missing";
+                  }
+                  return null;
+                },
+              ),
+            ],
+          )
+        ),
+      ),
     );
   }
 }
